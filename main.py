36e2b9c4f8097254c864f979e7741529e478665b
DEV310107 <mateusr110211@gmail.com>
2025-02-26 16:17:37 -0300

    save

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ with open("produtos.csv", "w", newline="", encoding="utf-8") as arquivo:
 with open("ordem_de_venda.csv", "w", newline="", encoding="utf-8") as arquivo:
     escritor = csv.writer(arquivo)
     escritor.writerows(ordem_de_venda)
+
+
+@app.route("/clientes", methods=["get"])
+def cliente():
+    return clientes
+
+@app.route("/produtos", methods=["get"])
+def produto():
+    return produtos
+
+@app.route("/ordem_de_venda")
